553293f320288f826276c30821870db70e730985
raphael4008 <raphaelosindi4008@gmail.com>
2025-02-19 14:13:38 +0300

    Initial Project Setup and Structure

---
+++ ELearning/fdback.php
@@ -0,0 +1,31 @@
+    <!-- Start Students Testimonial -->
+    <div class="container-fluid mt-5" style="background-color: #4B7289" id="Feedback">
+        <h1 class="text-center testyheading p-4"> Student's Feedback </h1>
+        <div class="row">
+          <div class="col-md-12">
+            <div id="testimonial-slider" class="owl-carousel">
+            <?php 
+              $sql = "SELECT s.stu_name, s.stu_occ, s.stu_img, f.f_content FROM student AS s JOIN feedback AS f ON s.stu_id = f.stu_id";
+              $result = $conn->query($sql);
+              if($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()){
+                  $s_img = $row['stu_img'];
+                  $n_img = str_replace('../','',$s_img)
+            ?>
+              <div class="testimonial">
+                <p class="description">
+                <?php echo $row['f_content'];?>  
+                </p>
+                <div class="pic">
+                  <img src="<?php echo $n_img; ?>" alt=""/>
+                </div>
+                <div class="testimonial-prof">
+                  <h4><?php echo $row['stu_name']; ?></h4>
+                  <small><?php echo $row['stu_occ']; ?></small>
+                </div>
+              </div>
+              <?php }} ?>
+            </div>
+          </div>
+        </div>
+    </div>
